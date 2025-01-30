563e6d735f2309ea9ae1f042369351ffe6dbfaf9
Thong Huynh <thuynh808@hotmail.com>
2025-01-30 09:21:23 -1000

    updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module "alb" {
   source         = "./modules/alb"
   project_name   = var.project_name
   vpc_id         = module.vpc.vpc_id
-  private_subnet = module.vpc.private_subnet_id
+  private_subnet = var.private_subnet_cidr
 }
 
 module "ecs" {
