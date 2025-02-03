c435b231cc8755b02cf0ab7b65df90c64b8887a6
Thong Huynh <thuynh808@hotmail.com>
2025-02-02 17:17:08 -1000

    created cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BUCKET_NAME="breach-tracker-s3"
+
+echo "Emptying and deleting S3 bucket: $BUCKET_NAME..."
+aws s3 rm "s3://$BUCKET_NAME" --recursive && aws s3 rb "s3://$BUCKET_NAME"
+
+if [ $? -eq 0 ]; then
+    echo "✅ Bucket $BUCKET_NAME successfully deleted."
+else
+    echo "Failed to delete bucket $BUCKET_NAME"
+fi
+
+cd terraform
+
+terraform destroy -var-file=myvars.tfvars -auto-approve
